0087c8d4c56feaa5598b4b3f71b962a19e2b6d7a
mohamedm1 <63683274+mohamedm1@users.noreply.github.com>
2020-08-29 13:02:53 -0400

    Create exercise3.java

---
+++ exercise3.java
@@ -0,0 +1,18 @@
+exercise 3 
+
+
+a) What is the root node of the tree?
+
+- 50 Nodes. 
+
+b) What are the leaf nodes of the tree?
+
+- leaf nodes are 9,14,19,67,76.
+
+c) What are the ancestor nodes of the node containing 19?
+
+- he node 19 is 23,17,50.
+
+d) Write the sequence of node values that you would get from a post-order traversal.
+
+-  the answer is 9,14.19,67,76,23,17,50.
